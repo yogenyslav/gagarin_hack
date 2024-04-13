4682d7602850fc60f73cdbb9a2871e56be5055fb
Yaroslav Romanenko <fropych@gmail.com>
2024-04-14 00:06:48 +0300

    resnet

--- ml/model.py
+++ ml/model.py
@@ -9,15 +9,7 @@ from sklearn.preprocessing import MinMaxScaler
 
 class Model(abc.ABC):
     @abc.abstractmethod
-    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
-        pass
-
-    @abc.abstractmethod
     def predict(self, X: np.ndarray) -> np.ndarray:
-        pass
-
-    @abc.abstractmethod
-    def predict_proba(self, X: np.ndarray) -> np.ndarray:
         pass
 
     @abc.abstractmethod
@@ -81,3 +73,68 @@ class LogReg(Model):
     @property
     def classes(self):
         return ["not_anomaly", "blur"]
+
+import torch
+from torch import nn
+import torchvision.models as models
+from sklearn.preprocessing import LabelEncoder
+
+class ResNet(nn.Module):
+    def __init__(self, n_class=2):
+        super(ResNet, self).__init__()
+        
+        self.resnet = models.resnet18(pretrained=True)
+        self.resnet.fc = torch.nn.Identity()
+        
+        self.fc = nn.Sequential(
+            nn.Linear(512, 256,),
+            nn.ReLU(),
+            nn.Linear(256, n_class)
+        )
+        
+    def forward(self, x):
+        batch_size, seq_size = x.shape[:2]
+        x = self.resnet(x.reshape(batch_size*seq_size, 3, 224, 224))
+        x = x.reshape(batch_size, seq_size, 512)
+        x = x.mean(1)
+        x = self.fc(x)
+        return x
+
+class DLModel(Model):
+    def __init__(self,) -> None:
+        self._model = ResNet(5).to('cpu')
+        self._le = LabelEncoder()
+        self.model.eval()
+
+    @property
+    def model(self):
+        return self._model
+
+    @property
+    def le(self):
+        return self._le
+
+    @torch.no_grad
+    def predict(self, X: torch.Tensor) -> np.ndarray:
+        return self.le.inverse_transform(self.model(X).argmax(1))
+
+    def save(self, dir: str) -> None:
+        dir = Path(dir)
+        
+        with open(dir / "model.pt", "wb") as f:
+            torch.save(self.model, f)
+
+        with open(dir / "le.pkl", "wb") as f:
+            pickle.dump(self.le, f)
+
+    def load(self, dir: str) -> None:
+        dir = Path(dir)
+        with open(dir / "model.pt", "rb") as f:
+            self.model.load_state_dict(torch.load(f))
+
+        with open(dir / "le.pkl", "rb") as f:
+            self._le = pickle.load(f)
+
+    @property
+    def classes(self):
+        return self.le.classes_
