e0f2b53fc7d267e2811f98ddb8aa88fb3f046430
Kasuich <kasui3108@gmail.com>
2024-04-13 21:16:07 +0300

    Beautify create_dataset.py

--- ml/create_dataset.py
+++ ml/create_dataset.py
@@ -9,6 +9,15 @@ import pandas as pd
 
 
 def get_frame(idx: int, fps: int, src: str, dst: str):
+    """
+    Convert video to binary frames
+
+    Args:
+        idx (int): id of the frame
+        fps (int): framerate of the video
+        src (str): path to video
+        dst (str): path to the result file
+    """
     start_sec = f"0{idx}"
     start_min = f"0{idx // 60}"
     if idx >= 60:
@@ -41,6 +50,13 @@ def get_frame(idx: int, fps: int, src: str, dst: str):
 
 
 def process_source(src: str, dst: str):
+    """
+    Convert video to binary frames
+
+    Args:
+        src (str): path to the video
+        dst (str): path to the result file
+    """
     probe = ffmpeg.probe(src)
     video_info = next(s for s in probe["streams"] if s["codec_type"] == "video")
     fps = int(video_info["r_frame_rate"].split("/")[0])
@@ -55,24 +71,20 @@ def select_chunk(
     chunk_duration: int,
     video_duration: int,
 ) -> tuple[int, int]:
+    """
+    Select random chunk from video
+
+
+    Args:
+        chunk_duration (int): chunk duration
+        video_duration (int): video duration
+
+    Returns:
+        tuple[int, int]: start and stop of chunk
+    """
     chunk_start = random.randint(0, video_duration - chunk_duration - 1)
     chunk_end = chunk_start + chunk_duration
     return chunk_start, chunk_end
-
-
-def get_frame_rate(video_file):
-
-    probe = ffmpeg.probe(video_file)
-
-    video_stream = next(
-        (stream for stream in probe["streams"] if stream["codec_type"] == "video"), None
-    )
-
-    if video_stream is not None and "r_frame_rate" in video_stream:
-        frame_rate = video_stream["r_frame_rate"]
-        return int(frame_rate.split("/")[0])
-    else:
-        return None
 
 
 def select_anomaly_subchunk(
@@ -80,32 +92,77 @@ def select_anomaly_subchunk(
     chunk_end: int,
     anomaly_duration: int,
 ) -> tuple[int, int]:
+    """
+    Select subchunk inside chunk
+
+    Args:
+        chunk_start (int): beginning of chunk
+        chunk_end (int): end of chunk
+        anomaly_duration (int): duration of anomal part in subchunk
+
+    Returns:
+        tuple[int, int]: start and stop of subchunk in seconds
+    """
     anomaly_start = random.randint(chunk_start, chunk_end - anomaly_duration - 1)
     anomaly_end = anomaly_start + anomaly_duration
     return anomaly_start, anomaly_end
 
 
-def apply_specific_highlight(frame, highlight_intensity):
+def apply_specific_highlight(frame, highlight_intensity: int):
+    """
+    Apply highlight anomaly to frame
+
+    Args:
+        frame: cv2 frame
+        highlight_intensity (int): highlight intencity
+
+    Returns:
+        processed cv2 frame
+    """
+
     highlighted_frame = cv2.addWeighted(
         frame, 1 + highlight_intensity / 10, np.zeros(frame.shape, frame.dtype), 0, 0
     )
     return highlighted_frame
 
 
-def apply_specific_blur(frame, blur_intensity):
+def apply_specific_blur(frame, blur_intensity: int):
+    """
+    Apply blur anomaly to frame
+
+    Args:
+        frame: cv2 frame
+        blur_intensity (int): blur intencity
+
+    Returns:
+        processed cv2 frame
+    """
     kernel_size = (blur_intensity * 2 + 1, blur_intensity * 2 + 1)
     blurred_frame = cv2.GaussianBlur(frame, kernel_size, 0)
     return blurred_frame
 
 
-def apply_specific_crop(frame, crop_width, crop_height, start_x, start_y):
+def apply_specific_crop(
+    frame, crop_width: int, crop_height: int, start_x: int, start_y: int
+):
+    """
+    Apply crop anomaly to frame
 
-    # Crop the frame
+    Args:
+        frame: cv2 frame
+        crop_width (int): crop width
+        crop_height (int): crop height
+        start_x (int): initial x-coord of crop
+        start_y (int): initial y-coord of crop
+
+    Returns:
+        processed cv2 frame
+    """
+
     cropped_frame = frame[
         start_y : start_y + crop_height, start_x : start_x + crop_width
     ]
 
-    # Upscale cropped frame to match initial width and height of the frame
     upscaled_frame = cv2.resize(
         cropped_frame, (frame.shape[1], frame.shape[0]), interpolation=cv2.INTER_NEAREST
     )
@@ -114,11 +171,36 @@ def apply_specific_crop(frame, crop_width, crop_height, start_x, start_y):
 
 
 def generate_noise_image(shape):
+    """
+    Generate noised image
+
+    Args:
+        shape: shape of target image
+
+    Returns:
+        noised image
+    """
     noise = np.random.randint(0, 255, shape, dtype=np.uint8)
     return noise
 
 
-def apply_specific_overlap(frame, radius, center_x, center_y, noise_image):
+def apply_specific_overlap(
+    frame, radius: int, center_x: int, center_y: int, noise_image
+):
+    """
+    Apply overlap anomaly to frame
+
+    Args:
+        frame: cv2 frame
+        radius (int): radius of overlapping circle
+        center_x (int): center of overlapping circle
+        center_y (int): center of overlapping circle
+        noise_image: noized image (circle filling)
+
+    Returns:
+        processed cv2 frame
+    """
+
     overlay = frame.copy()
     height, width = frame.shape[0], frame.shape[1]
 
@@ -135,9 +217,24 @@ def apply_specific_overlap(frame, radius, center_x, center_y, noise_image):
     return frame
 
 
-def apply_anomaly(video_capture, out, anomaly_start, anomaly_end, type, fps):
+def apply_anomaly(
+    video_capture, out, anomaly_start: int, anomaly_end: int, type: str, fps: int
+) -> pd.DataFrame:
+    """
+    Apply anomaly to video capture
+
+    Args:
+        video_capture: cv2 video capture
+        out: cv2 video writer
+        anomaly_start (int): start of anomaly (in seconds)
+        anomaly_end (int): end of anomaly (in seconds)
+        type (str): type of anomaly to be applied
+        fps (int): fps of capture
+
+    Returns:
+        (pd.DataFrame) dataframe with labeling
+    """
     video_duration = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT)) // fps
-    anomaly_duration = anomaly_end - anomaly_start
     intensity = random.randint(20, 30)
 
     frame_width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -155,7 +252,7 @@ def apply_anomaly(video_capture, out, anomaly_start, anomaly_end, type, fps):
         if type == "crop"
         else random.randint(0, frame_height)
     )
-    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
     radius = random.randint(frame_width // 4, frame_width // 2)
     anomalies = []
     for frame_number in range(video_duration * fps):
@@ -196,7 +293,7 @@ def apply_anomaly(video_capture, out, anomaly_start, anomaly_end, type, fps):
             anomalies.append(1)
         else:
             anomalies.append(0)
-    # Создание DataFrame
+
     df = pd.DataFrame(
         {
             "filename": [f"frame-{i}.bin" for i in range(video_duration)],
@@ -207,21 +304,32 @@ def apply_anomaly(video_capture, out, anomaly_start, anomaly_end, type, fps):
     return df
 
 
-def create_new_cap_from_end(cap, start_seconds, end_seconds, fps):
-    # Get total frames and frame rate of the original capture
-    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+def create_new_cap_from_end(cap, start_seconds: int, end_seconds: int, fps: int):
+    """
+    Create new cap from start and end in seconds
+
+    Args:
+        cap: initial cv2 video capture
+        start_seconds (int): start of new capture in seconds
+        end_seconds (int): end of new capture in seconds
+        fps (int): framerate
+
+    Returns:
+        new cv2 video capture
+    """
+
     frame_rate = fps
-    # Calculate the frame indices corresponding to start and end times
+
     start_frame = int(start_seconds * frame_rate)
     end_frame = int(end_seconds * frame_rate)
-    # Create a new video writer with the same properties as the original capture
+
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    fourcc = cv2.VideoWriter_fourcc(*"H264")  # Define the codec for MP4 format
+    fourcc = cv2.VideoWriter_fourcc(*"avc1")
 
     new_cap = cv2.VideoWriter(f"new_capture.mp4", fourcc, frame_rate, (width, height))
-    # Set the frame position to the start frame
+
     cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
     for i in range(start_frame, end_frame + 1):
         ret, frame = cap.read()
@@ -234,10 +342,18 @@ def create_new_cap_from_end(cap, start_seconds, end_seconds, fps):
 
 
 def create_directories(
-    output_dir,
-    n_chunks,
-    n_subchunks,
+    output_dir: str,
+    n_chunks: int,
+    n_subchunks: int,
 ):
+    """
+    Generate directories structure for dataset
+
+    Args:
+        output_dir (str): output directory
+        n_chunks (int): number of chunks
+        n_subchunks (int): number of subchunks
+    """
 
     for i in range(n_chunks):
         for j in range(n_subchunks):
@@ -247,22 +363,38 @@ def create_directories(
 
 
 def process_one_chunk(
-    chunk_start,
-    chunk_end,
+    chunk_start: int,
+    chunk_end: int,
     video_capture,
-    chunk_duration,
-    anomaly_duration,
-    type,
-    out_path,
-    fps,
-):
+    chunk_duration: int,
+    anomaly_duration: int,
+    type: str,
+    out_path: str,
+    fps: int,
+) -> pd.DataFrame:
+    """
+    Process one chunk of video capture
+
+    Args:
+        chunk_start (int): start of chunk in seconds
+        chunk_end (int): end of chunk in seconds
+        video_capture: cv2 video capture
+        chunk_duration (int): chunk duration in seconds
+        anomaly_duration (int): anomaly duration in seconds
+        type (str): type of anomaly to be generated
+        out_path (str): output dir
+        fps (int): framerate of video capture
+
+    Returns:
+        (pd.DataFrame): labels dataframe
+    """
 
     frame_width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
     new_cap = create_new_cap_from_end(video_capture, chunk_start, chunk_end, fps)
     out = cv2.VideoWriter(
         out_path,
-        cv2.VideoWriter_fourcc(*"H264"),
+        cv2.VideoWriter_fourcc(*"avc1"),
         fps,
         (frame_width, frame_height),
     )
@@ -283,7 +415,22 @@ def process_one_video(
     chunk_duration: int,
     anomaly_duration: int,
     type: str,
-):
+) -> pd.DataFrame:
+    """
+    Process one video
+
+    Args:
+        input_path (str): path to video to be processed
+        output_path (str): path to dataset to be saved
+        n_chunks (int): number of chunks
+        n_subchunks (int): number of subchunks
+        chunk_duration (int): chunk duration in seconds
+        anomaly_duration (int): anomaly duration in seconds
+        type (str): type of anomaly to be applied
+
+    Returns:
+        pd.DataFrame: labeling dataframe
+    """
     cap = cv2.VideoCapture(input_path)
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     output_path = Path(output_path)
@@ -347,14 +494,15 @@ def process_one_video(
 
 
 if __name__ == "__main__":
-    input_path = Path("./Train/not_anomaly/")
-    outp_path = Path("./final_dataset/")
+    # пример генерации датасета
+    input_path = Path("./data/Train/not_anomaly/")
+    outp_path = Path("./data/final_dataset2/")
     N_CHUNKS = 10
     N_SUBCHUNKS = 4
     CHUNK_DURATION = 10
     ANOMALY_DURATION = 3
 
-    anomalies_types = ["highlight", "blur"]
+    anomalies_types = ["highlight", "blur", "crop", "overlap"]
 
     type_df = pd.DataFrame()
     for type in anomalies_types:
