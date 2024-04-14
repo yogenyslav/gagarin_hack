d8ce4b15ab56e5bb7efca4ba0cb30aa30d81fdfa
ShmatEugene <shmat.evg@yandex.ru>
2024-04-14 05:02:00 +0300

    front: улучшить вид гифки

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 // export const API_URL = 'itatmisis1.ru';
-export const API_URL = 'https://api.dino-misis.ru';
-// export const API_URL = 'https://ccc1-95-140-147-162.ngrok-free.app';
+// export const API_URL = 'https://api.dino-misis.ru';
+export const API_URL = 'https://ccc1-95-140-147-162.ngrok-free.app';
